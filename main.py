edd7a34ab46fca6c57709ac11243232b3bac6960
Romulo <romin.nf@gmail.com>
2026-07-27 21:16:22 -0300

    prompts and function calls to get_files info

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
+import json
 import os
-from pydoc import cli
 
 from dotenv import load_dotenv
 from openai import OpenAI
 
+from functions.call_function import available_functions
 from prompts import system_prompt
 
 
@@ -21,13 +22,19 @@ def returning_response(client, model, args):
         {"role": "system", "content": system_prompt},
         {"role": "user", "content": args.user_prompt},
     ]
-    response = client.chat.completions.create(model=model, messages=messages)
+    response = client.chat.completions.create(
+        model=model, messages=messages, tools=available_functions
+    )
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         if response.usage is not None:
             print(f"Prompt tokens: {response.usage.prompt_tokens}")
             print(f"Response tokens: {response.usage.completion_tokens}")
     print(f"Response:\n {response.choices[0].message.content}")
+    message = response.choices[0].message
+    for tool_call in message.tool_calls:
+        function_args = json.loads(tool_call.function.arguments or "{}")
+        print(f"Calling function: {tool_call.function.name}({function_args})")
 
 
 def main():
